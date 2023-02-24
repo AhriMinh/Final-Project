ea7ad77741010fa74f0b516ddf518ac030e825bf
ShujinOfAhri <duongminhquang11357@gmail.com>
2023-02-24 23:46:52 +0700

    update

--- js/clock.js
+++ js/clock.js
@@ -59,30 +59,3 @@ function updateTime() {
   setInterval(function() {
     updateTime()
   }, 1000);
-
-
-
-
-  var boxContainer = document.querySelector('.box-container');
-  var boxImage = document.querySelector('.box-image');
-  var cardImage = document.querySelector('.card-image');
-
-// Lấy thẻ button và bắt sự kiện click chuột
-  var showBoxButton = document.getElementById('show-box');
-
-  showBoxButton.addEventListener('click', function() {
-      boxImage.classList.add('shake');
-      setTimeout(() => {
-      boxImage.classList.remove('shake');
-      }, 3000);
-  });
-
-
-          // Bắt sự kiện click vào nút bấm
-  showBoxButton.addEventListener('click', function() {
-
-  // Hiển thị hình ảnh
-      cardImage.style.transform = 'translateY(0)';
-  });
-
-
